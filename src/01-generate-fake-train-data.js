112872176c8201eb3d11ec1747796ed0c2580fcb
Julian Adler <julian.dot.adler@gmail.com>
2023-05-28 13:38:58 +1200

    I was told to leave it alone, but I have this thing called OCD, you see

--- src/01-generate-fake-train-data.js
+++ src/01-generate-fake-train-data.js
@@ -2,7 +2,7 @@ import { randomBool } from "./tools/random.js";
 import { max, diff, mean, randomEntry } from "./tools/array.js";
 import { writeObject } from "./tools/files.js";
 
-const TRAIN_SAMPLES = 10000;
+const TRAIN_SAMPLES = 100000;
 const TEST_SAMPLES = 2000;
 const SPEEDS_PER_SAMPLES = 50;
 
