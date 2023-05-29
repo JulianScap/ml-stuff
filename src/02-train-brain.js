7909952271ecd4a581c3f1cf84cd6803ec3cdb1b
Julian Adler <julian.dot.adler@gmail.com>
2023-05-29 20:14:07 +1200

    rc

--- src/02-train-brain.js
+++ src/02-train-brain.js
@@ -22,29 +22,32 @@ const outputSize = trainMatter[0].output.length;
 const neuronRatio = 2 / 3;
 
 const height = Math.floor((inputSize + outputSize) * neuronRatio);
-const hiddenLayers = 3; // in range [1, 3]
+const hiddenLayers = 4; // in range [1, 3]
 const options = {
   hiddenLayers: new Array(hiddenLayers).fill(height),
 };
 
+console.log(`Started at ${new Date().toLocaleString()}`);
 console.log(`Creating a ${networkType} with:`);
 console.log(`\tInput Size: ${inputSize}`);
 console.log(`\tOutput Size: ${outputSize}`);
 console.log(`\tHeight: ${height}`);
 console.log(`\tHiddenLayers: ${hiddenLayers}`);
 console.log(`\tTrainingData: ${trainMatter.length}`);
+console.log(`\tCross Validation? ${crossTrain} (K=${k})`);
 
 let net = null;
 let buildNetwork = null;
 switch (networkType) {
   case NetworkTypes.FeedForward:
+    // Not working, can't export and import
     buildNetwork = () =>
       new FeedForward({
         inputLayer: () => input({ height: 50 }),
         hiddenLayers: new Array(hiddenLayers).fill((inputLayer) =>
           feedForward({ height: height }, inputLayer)
         ),
-        outputLayer: (inputLayer) => target({ height: 4 }, inputLayer),
+        outputLayer: (inputLayer) => target({ height: 1 }, inputLayer),
         praxisOpts: {
           decayRate: 0.99,
         },
@@ -64,6 +67,9 @@ switch (networkType) {
 if (crossTrain) {
   const crossValidate = new CrossValidate(buildNetwork);
   crossValidate.train(trainMatter, trainSettings, k);
+
+  const crossValidateJson = crossValidate.toJSON();
+  await writeObject(crossValidateJson, 'crossValidate.json');
 
   net = crossValidate.toNeuralNetwork();
 } else {
